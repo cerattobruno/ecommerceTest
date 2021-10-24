5a0d7a842dfc6263da40e1b02371402bd56fd83a
Bruno Ceratto <cerattobruno@gmail.com>
2021-10-24 10:35:24 -0300

    Se maqueto sección Navbar y Footer.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { StoreComponent } from './components/pages/store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tienda', component: StoreComponent },
+  { path: 'home', component: HomeComponent,  pathMatch: 'full' },
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { StoreComponent } from './components/pages/store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
